366f397db8b2ca211eb56bd4987bdc37877a4453
Epiq122 <l2ob122@gmail.com>
2022-07-12 21:11:11 -0700

    finished working on more of the top nav, and added styles

--- src/playground.js
+++ src/playground.js
@@ -1,28 +1,54 @@
-// ES6 Array Spreading
-// ... spreads all the elements in the array
+// // ES6 Array Spreading
+// // ... spreads all the elements in the array
 
-const fruits = ["apple", "orange", "grape"];
-const veggies = ["cucumber", "potato"];
+// const fruits = ["apple", "orange", "grape"];
+// const veggies = ["cucumber", "potato"];
 
-// combines all the elements together
-const combined = [...fruits, ...veggies];
-console.log(combined);
+// // combines all the elements together
+// const combined = [...fruits, ...veggies];
+// console.log(combined);
 
-//Javascript Objects, Properties and Methods
+// //Javascript Objects, Properties and Methods
 
-const developer = {
-  salary: 100000,
-  experience: 1,
-  techStack: ["vue", "html", "css"],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
-  },
-};
-console.log(developer.salary);
-console.log(developer.lookingForWork);
+// const developer = {
+//   salary: 100000,
+//   experience: 1,
+//   techStack: ["vue", "html", "css"],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary = this.salary * 2;
+//     this.lookingForWork = false;
+//   },
+// };
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
 
-developer.doubleSalary();
-console.log(developer.salary);
-console.log(developer.lookingForWork);
+// developer.doubleSalary();
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
+
+// // Working with Tests
+// export const evenOrOdd = (number) => {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+
+// export const multiply = (num1, num2) => {
+//   // let total = 0;
+//   // for (let i = 0; i < num1; i++) {
+//   //   total += num2;
+//   // }
+//   // return total;
+//   return num1 * num2;
+// };
+
+// Map Method
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map((number) => {
+  return number * number;
+});
+console.log(squares);
---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,24 @@
+import { mount } from "@vue/test-utils";
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    const wrapper = mount(MainNav);
+    expect(wrapper.text()).toMatch("Epiq Jobs");
+  });
+  it("displays menu items in navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Epiq",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
+});
