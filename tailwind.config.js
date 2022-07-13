f6f32cc30e4d85153ae6a8f01190339e98c4ba98
Epiq122 <l2ob122@gmail.com>
2022-07-12 21:22:29 -0700

    created action button component and began styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
@@ -8,7 +7,7 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
-      color: {
+      colors: {
         "brand-gray-1": "#dadce0",
         "brand-blue-1": "#1967d2",
         "brand-green-1": "#137333",
