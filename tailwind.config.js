d1afd7e9152e968c25127516de029e3dcdb2eb29
Robert Gleason <l2ob122@gmail.com>
2024-01-21 13:30:59 -0800

    Add Google Fonts link and extend font family in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 // tailwind.config.js
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {
     extend: {}
