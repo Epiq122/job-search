02a323017eeb79a280d5fdce4a2cbde33de08cdd
Robert Gleason <l2ob122@gmail.com>
2024-01-21 13:51:05 -0800

    Add MainNav component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav
+  }
+}
+</script>
+
 <template>
-  <h1 class="mt-8 text-center text-6xl">Welcome to my Nightmare</h1>
+  <nav>
+    <MainNav />
+  </nav>
 </template>
