efa99f1d608b1046b6ce23ceaba41c289567225e
Robert Gleason <l2ob122@gmail.com>
2024-01-22 15:56:58 -0800

    Refactor TheHero.vue component to add a yellow background and adjust section styling, background set for testing purposes

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -6,6 +6,8 @@ export default {
 
 <template>
   <main>
-    <section>and they say that a HERO will save us</section>
+    <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
+      and they say that a HERO will save us
+    </section>
   </main>
 </template>
