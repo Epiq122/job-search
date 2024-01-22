16f35e0a3f6b8dd834d846cf9f1f140665b09384
Robert Gleason <l2ob122@gmail.com>
2024-01-21 19:11:05 -0800

    Add company name to MainNav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,14 +1,19 @@
 <script>
 export default {
-  name: 'MainNav'
-}
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Epiq Careers'
+    };
+  }
+};
 </script>
 
 <template>
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="flex-no-wrap border-brand-gray-1 mx-auto flex h-full border-b border-solid px-8">
-        <a href="/" class="flex h-full items-center text-xl">Epiq Careers</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
