8dddf77c5780efc81102cd39913dbe5611914a1f
Robert Gleason <l2ob122@gmail.com>
2024-01-21 19:02:18 -0800

    Update prettier configuration and add code examples

--- src/playground.js
+++ src/playground.js
@@ -1,7 +1,26 @@
-const fruits = ['apple', 'orange', 'banana', 'grapes', 'mango', 'kiwi']
-const vegetables = ['potato', 'tomato', 'onion', 'cabbage', 'cauliflower', 'broccoli']
-const items = [...fruits, vegetables] // spread operator
-const allItems = [...fruits, ...vegetables] // spread operator
+const fruits = ['apple', 'orange', 'banana', 'grapes', 'mango', 'kiwi'];
+// spread operator
 
-console.log(items)
-console.log(allItems)
+const vegetables = ['potato', 'tomato', 'onion', 'cabbage', 'cauliflower', 'broccoli'];
+const items = [...fruits, vegetables]; // spread operator
+const allItems = [...fruits, ...vegetables]; // spread operator
+
+console.log(items);
+console.log(allItems);
+console.log('---------------------');
+// objects properties and methods
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ['React', 'Node', 'MongoDB', 'Express'],
+  lookingForWork: true,
+  doubleSalary() {
+    return this.salary * 2;
+  }
+};
+
+console.log(developer.salary);
+console.log(developer.experience);
+console.log(developer.techStack);
+console.log(developer.lookingForWork);
+console.log(developer.doubleSalary());
