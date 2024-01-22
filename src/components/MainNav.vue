b065b685b2b86817b0e385b1aed8caffafbda8d4
Robert Gleason <l2ob122@gmail.com>
2024-01-21 21:23:24 -0800

    Add navigation links to MainNav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -15,6 +15,28 @@ export default {
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="flex-no-wrap border-brand-gray-1 mx-auto flex h-full border-b border-solid px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Life at {{ company }}</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">How we hire</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
