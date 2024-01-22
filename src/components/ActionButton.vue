b73fbd5a0f1cdc6ca2790b4ada1854861e7dca42
Robert Gleason <l2ob122@gmail.com>
2024-01-22 09:37:49 -0800

    Add click event handler to Sign In button

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,12 +1,18 @@
 <script>
 export default {
   name: 'ActionButton',
+  methods: {
+    handleClick() {
+      console.log('Sign In');
+    },
+  },
 };
 </script>
 
 <template>
   <button
     class="hover:shadow-blue rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white"
+    @click="handleClick"
   >
     Sign In
   </button>
