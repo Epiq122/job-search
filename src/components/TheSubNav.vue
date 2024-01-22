9ee7c892f1d9aefc52f5dc3cf72efd4a8c722307
Robert Gleason <l2ob122@gmail.com>
2024-01-22 15:15:09 -0800

    Add Font Awesome icons and update subnav component

--- src/components/TheSubNav.vue
+++ src/components/TheSubNav.vue
@@ -5,5 +5,13 @@ export default {
 </script>
 
 <template>
-  <div>subnav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">
+      <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="mr-3" />
+      <span>
+        <span class="text-brand-green-1">1986</span>
+        jobs matched
+      </span>
+    </div>
+  </div>
 </template>
