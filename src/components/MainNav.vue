85e8725f9821585e775f4105bcf15f3150f70ccf
Robert Gleason <l2ob122@gmail.com>
2024-01-21 21:19:09 -0800

    Update MainNav.vue with company URL using the vbind directive

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,7 +3,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Epiq Careers'
+      company: 'Epiq Careers',
+      url: 'https://careers.google.com'
     };
   }
 };
@@ -13,7 +14,7 @@ export default {
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="flex-no-wrap border-brand-gray-1 mx-auto flex h-full border-b border-solid px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
