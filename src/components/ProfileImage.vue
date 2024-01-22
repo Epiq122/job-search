4ecba02ae0ea99d92adfa8473abb8808e56bad57
Robert Gleason <l2ob122@gmail.com>
2024-01-22 09:21:38 -0800

    Add ProfileImage component with profile picture

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'ProfileImage',
+  data() {
+    return {
+      profileImage: '@/images/profile.jpg',
+    };
+  },
+};
+</script>
+
+<template><img class="h-8 w-8 rounded-3xl object-contain" :src="profileImage" /></template>
