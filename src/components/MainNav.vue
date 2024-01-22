07c9b8ae43b2f4f89e3c551826825200094284b1
Robert Gleason <l2ob122@gmail.com>
2024-01-21 18:56:59 -0800

    Update MainNav.vue with new header design

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,5 +5,11 @@ export default {
 </script>
 
 <template>
-  <header class="w-full text-sm">Main Nav</header>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="flex-no-wrap border-brand-gray-1 mx-auto flex h-full border-b border-solid px-8">
+        <a href="/" class="flex h-full items-center text-xl">Epiq Careers</a>
+      </div>
+    </div>
+  </header>
 </template>
