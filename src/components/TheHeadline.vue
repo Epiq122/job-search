b76547b32650260e4a0e3d4fc11f1ff4454ba8fe
Robert Gleason <l2ob122@gmail.com>
2024-01-22 17:05:06 -0800

    Add actionClasses computed property and corresponding styles

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -7,6 +7,16 @@ export default {
       interval: null,
     };
   },
+  computed: {
+    actionClasses() {
+      return {
+        build: this.action === 'Build',
+        create: this.action === 'Create',
+        design: this.action === 'Design',
+        code: this.action === 'Code',
+      };
+    },
+  },
   created() {
     this.changeTitle();
   },
@@ -28,7 +38,24 @@ export default {
 
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
+    <h1>
+      <span :class="actionClasses">{{ action }}</span> for everyone
+    </h1>
     <h2>Find your next job at Epiq Careers</h2>
   </section>
 </template>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
