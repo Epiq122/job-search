63608c880b7e0f9020a5efd2d36f191688a7dbcd
Robert Gleason <l2ob122@gmail.com>
2024-01-22 16:52:25 -0800

    Add TheHeadline component and import it in TheHero.vue

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'TheHeadline',
+  data() {
+    return {
+      sample: 'Build for everyone',
+    };
+  },
+  created() {
+    console.log('TheHeadline.vue created', this.sample);
+  },
+};
+</script>
+
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Epiq Careers</h2>
+  </section>
+</template>
