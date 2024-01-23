78e997cb2e990cf0061585559a914a27f89b2c92
Robert Gleason <l2ob122@gmail.com>
2024-01-22 17:00:03 -0800

    Refactor TheHeadline component to dynamically change action in the title

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -3,18 +3,32 @@ export default {
   name: 'TheHeadline',
   data() {
     return {
-      sample: 'Build for everyone',
+      action: 'Build',
+      interval: null,
     };
   },
   created() {
-    console.log('TheHeadline.vue created', this.sample);
+    this.changeTitle();
+  },
+  beforeUnMount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ['Build', 'Create', 'Design', 'Code'];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1) % actions.length;
+        this.action = actions[nextActionIndex];
+      }, 3000);
+    },
   },
 };
 </script>
 
 <template>
   <section>
-    <h1>Build for everyone</h1>
+    <h1>{{ action }} for everyone</h1>
     <h2>Find your next job at Epiq Careers</h2>
   </section>
 </template>
