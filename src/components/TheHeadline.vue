eb0fce152c8feabed9ac5870d31eb77c6924b612
Robert Gleason <l2ob122@gmail.com>
2024-01-22 17:07:16 -0800

    Refactor headline styling in TheHeadline.vue

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -38,10 +38,12 @@ export default {
 
 <template>
   <section>
-    <h1>
-      <span :class="actionClasses">{{ action }}</span> for everyone
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
     </h1>
-    <h2>Find your next job at Epiq Careers</h2>
+    <h2 class="text-3xl font-light">Find your next job at Epiq Careers</h2>
   </section>
 </template>
 
