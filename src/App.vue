28760d921d7743bb51547ed1a6961e2429b2d1cc
Robert Gleason <l2ob122@gmail.com>
2024-01-23 08:09:12 -0800

    refactored folder steructure and fixed import for nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import MainNav from '@/components/MainNav.vue';
+import MainNav from '@/components/navigation/MainNav.vue';
 import TheHero from '@/components/TheHero.vue';
 
 export default {
