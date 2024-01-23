11c5453ed7d2ed496057fbc78742ed35eb156ac4
Robert Gleason <l2ob122@gmail.com>
2024-01-22 16:04:42 -0800

    Add grid layout to TheHero component ( added blue boxes to for templating purposes)

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -7,7 +7,12 @@ export default {
 <template>
   <main>
     <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
-      and they say that a HERO will save us
+      <div class="grid grid-cols-12">
+        <div class="border-blue col-span-1 col-start-1 border border-blue-700">1 column</div>
+        <div class="border-blue col-span-5 col-start-2 border border-blue-700">5 column</div>
+        <div class="border-blue col-span-5 col-start-7 border border-blue-700">5 column</div>
+        <div class="border-blue col-span-1 col-start-12 border border-blue-700">1 column</div>
+      </div>
     </section>
   </main>
 </template>
