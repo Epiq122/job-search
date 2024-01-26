3d12e6aabafc204bfd4e898c74e3e0c7d703a470
Robert Gleason <l2ob122@gmail.com>
2024-01-26 11:21:45 -0800

    Add navigation links to App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,9 @@ export default {
 </script>
 
 <template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="jobs/results">Job Results</router-link>
+  </div>
   <router-view />
 </template>
