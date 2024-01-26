1246d89f6b54150f03f69691665fe48a03d59183
Robert Gleason <l2ob122@gmail.com>
2024-01-26 11:18:24 -0800

    Remove unused components and update template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script>
-import MainNav from '@/components/navigation/MainNav.vue';
-import TheHero from '@/components/jobsearch/TheHero.vue';
-
 export default {
   name: 'App',
-  components: {
-    MainNav,
-    TheHero,
-  },
 };
 </script>
 
 <template>
-  <main-nav />
-  <the-hero />
+  <router-view />
 </template>
