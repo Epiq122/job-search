fbb3020460cb28734fb429b7e7eb6f85a7e8a4a8
Robert Gleason <l2ob122@gmail.com>
2024-01-26 11:35:48 -0800

    Add go to Home Page button in JobResultsView.vue

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,11 +1,17 @@
 <script>
 export default {
   name: 'JobResultsView',
+  methods: {
+    goToHome() {
+      this.$router.push({ name: 'Home' });
+    },
+  },
 };
 </script>
 
 <template>
   <div>
     <h1 class="text-6xl font-bold">Job Results Page</h1>
+    <button @click="goToHome">Go to Home Page</button>
   </div>
 </template>
